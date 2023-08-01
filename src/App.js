0f20d7b9aada5212e95202c5432248a7b86a41ad
haka <alexsveshnik@gmail.com>
2023-08-01 21:34:27 +0300

    lesson03-pizza-app done!

--- src/App.js
+++ src/App.js
@@ -69,21 +69,21 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
+
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map((p) => (
-            <Pizza pizzaObj={p} key={p.name} />
-          ))}
-          {/* {pizzaData.map((p) => (
-          <Pizza
-            name={p.name}
-            ingredients={p.ingredients}
-            price={p.price}
-            photoName={p.photoName}
-            soldOut={p.soldOut}
-          />
-        ))} */}
-        </ul>
+        // обернули код в фрагмент реакт <>, чтобы можно было писать более 1-го элемента jsx
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+
+          <ul className="pizzas">
+            {pizzaData.map((p) => (
+              <Pizza pizzaObj={p} key={p.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later.</p>
       )}
@@ -97,7 +97,8 @@ function Pizza({ pizzaObj }) {
   // if (props.pizzaObj.soldOut) return null;
 
   return (
-    <li className={!pizzaObj.soldOut ? "pizza" : "pizza sold-out"}>
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      {/* <li className={!pizzaObj.soldOut ? "pizza" : "pizza sold-out"}> */}
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
