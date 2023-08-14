3a037b38c147d8a082698fb940ef2ad830b76fe2
haka <alexsveshnik@gmail.com>
2023-08-14 21:52:06 +0300

    lesson07-travel-list filter items

--- src/App.js
+++ src/App.js
@@ -89,10 +89,29 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  // filter <div className="actions">
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {/* {items.map((item) => ( */}
+        {/* заменяем обычные items на sortedItems, чтобы работала фильтрация */}
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -101,6 +120,14 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
