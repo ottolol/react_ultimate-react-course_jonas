ebc9d8689662dbdbbebf971736168bc04a67df2e
haka <alexsveshnik@gmail.com>
2023-09-18 22:15:55 +0300

    вывел табы через .map() и добавил id

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ const content = [
     summary: "React is a library for building UIs",
     details:
       "Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    id: 0,
   },
   {
     summary: "State management is like giving state a home",
     details:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    id: 1,
   },
   {
     summary: "We can think of props as the component API",
     details:
       "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    id: 2,
   },
 ];
 
@@ -37,6 +40,9 @@ function Tabbed({ content }) {
         <Tab num={1} activeTab={activeTab} onClick={setActiveTab} />
         <Tab num={2} activeTab={activeTab} onClick={setActiveTab} />
         <Tab num={3} activeTab={activeTab} onClick={setActiveTab} />
+        {/* {content.map((c) => (
+          <Tab id={c.id} activeTab={activeTab} onClick={setActiveTab} />
+        ))} */}
       </div>
 
       {activeTab <= 2 ? (
@@ -101,4 +107,4 @@ function DifferentContent() {
 }
 
 // D:\Из Торрента\The Ultimate React Course 2023 React, Redux & More\11. How React Works Behind the Scenes
-// 124 Components, Instances, and Elements
+// 127 How Rendering Works The Render Phase
