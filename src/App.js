13d19d3c328f132facf789e91f9f93ee076b98e4
ottolol <alexsveshnik@gmail.com>
2023-12-01 22:07:24 +0300

    l16 - добавили controller для отмены  запросов

--- src/App.js
+++ src/App.js
@@ -84,13 +84,16 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
 
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok)
@@ -102,7 +105,9 @@ export default function App() {
           setMovies(data.Search);
           setError("");
         } catch (err) {
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -115,6 +120,10 @@ export default function App() {
       }
 
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
